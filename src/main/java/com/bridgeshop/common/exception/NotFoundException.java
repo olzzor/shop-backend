f90bdddfa955ca7c80263897fce81a3728dce372
olzzor <10dokey@naver.com>
2024-02-02 18:47:53 +0900

    2023/02/02 주석 추가

--- src/main/java/com/bridgeshop/common/exception/NotFoundException.java
+++ src/main/java/com/bridgeshop/common/exception/NotFoundException.java
@@ -5,32 +5,66 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Map;
 
+/**
+ * 요청한 리소스를 찾을 수 없을 때 발생하는 예외입니다.
+ * 이 예외는 404 NOT FOUND 상태 코드의 HTTP 응답을 생성합니다.
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
     private final String errorCode;
     private Map<String, Object> body;
 
+
+    /**
+     * 지정된 오류 코드 및 세부 메시지로 새 NotFoundException 을 구성합니다.
+     *
+     * @param errorCode 오류 코드.
+     * @param message   세부 메시지.
+     */
     public NotFoundException(String errorCode, String message) {
         super(message);
         this.errorCode = errorCode;
     }
 
+    /**
+     * 지정된 오류 코드, 세부 메시지 및 본문으로 새 NotFoundException 을 구성합니다.
+     *
+     * @param errorCode 오류 코드.
+     * @param message   세부 메시지.
+     * @param body      응답 본문.
+     */
     public NotFoundException(String errorCode, String message, Map<String, Object> body) {
         super(message);
         this.errorCode = errorCode;
         this.body = body;
     }
 
+    /**
+     * 지정된 세부 메시지와 원인으로 새 NotFoundException 을 구성합니다.
+     *
+     * @param message 세부 메시지.
+     * @param cause   원인.
+     */
     public NotFoundException(String message, Throwable cause) {
         super(message, cause);
         this.errorCode = "";
     }
 
+    /**
+     * 이 예외와 연결된 오류 코드를 반환합니다.
+     *
+     * @return 오류 코드.
+     */
     public String getErrorCode() {
         return errorCode;
     }
 
+    /**
+     * 이 예외와 연결된 응답 본문을 반환합니다.
+     *
+     * @return 응답 본문.
+     */
     public Map<String, Object> getBody() {
         return body;
     }
